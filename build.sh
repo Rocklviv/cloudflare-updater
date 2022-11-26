dc33805690cf0c7cf2d109e0f15c0625f13f4453
Rocklviv <dchekirda@gmail.com>
2022-11-26 10:52:49 +0100

    Fix

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-docker build -t ${2:-"rocklviv/cloudflare-dns-updater"} .
-docker login -u rocklviv -p dckr_pat_is5Nh4nnnM5zpadKOnneyMt9d58
-
-docker tag rocklviv/cloudflare-dns-updater:latest rocklviv/cloudflare-dns-updater:${1}
-docker push rocklviv/cloudflare-dns-updater:${1}
